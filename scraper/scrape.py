1db8653e071973d6d24ac7f85603a79e8e9c3c4c
George Pîrlea <george@dranov.ro>
2014-02-22 14:15:12 +0200

    Now parsing command line arguments

--- scraper/scrape.py
+++ scraper/scrape.py
@@ -1,47 +1,82 @@
 #! /usr/bin/python
 
 import requests
+import os
 import os.path
+import argparse
 import time
 
-url_fmt ='http://www.admitere.edu.ro/2013/staticRepI/j/{county}/cina/page_{page_num}.html'
-file_fmt = '{county}-{page_num}.html'
+admitere_url_fmt ='http://www.admitere.edu.ro/{year}/staticRepI/j/{county}/cina/page_{page_num}.html'
+bac_url_fmt = 'http://bacalaureat.edu.ro/{year}/rapoarte/{county}/rezultate/alfabetic/page_{page_num}.html'
+
+file_fmt = '{exam}-{year}-{county}-{page_num}.html'
 
 counties = ('AB', 'AG', 'AR', 'B', 'BC', 'BH', 'BN', 'BR', 'BT', 'BV', 'BZ',
 'CJ', 'CL', 'CS', 'CT', 'CV', 'DB', 'DJ', 'GJ', 'GL', 'GR', 'HD', 'HR', 'IF',
 'IL', 'IS', 'MH', 'MM', 'MS', 'NT', 'OT', 'PH', 'SB', 'SJ', 'SM', 'SV', 'TL',
 'TM', 'TR', 'VL', 'VN', 'VS')
 
+valid_exams = ('admitere', 'bacalaureat')
+
 
 if __name__ == '__main__':
+    # Parse command line arguments
+    parser = argparse.ArgumentParser()
 
-    for county in counties:
-        previous_status = 200
-        page_num = 1
+    parser.add_argument('--exam', help='admitere/bacalaureat', required=True)
+    parser.add_argument('--year', help='year to scrape data from',
+            required=True)
+    parser.add_argument('--county', help='county to scrape data for',
+            required=True)
 
-        while previous_status == 200:
-            print '[{0}] - page {1}'.format(county, page_num), 
-            # print "{0} out of {1}".format(page_num, NUM_PAGES), 
+    args = parser.parse_args()
 
-            start_time = time.clock()
+    if args.county not in counties:
+        print '{0} is not a county code'.format(args.county)
 
-            url = url_fmt.format(county=county, page_num=page_num)
-            filename = file_fmt.format(county=county, page_num=page_num)
+    if args.exam not in valid_exams:
+        print '{0} is not a valid exam name'.format(args.exam)
 
-            if not os.path.isfile(filename):
-                r = requests.get(url)
-                f = open(filename, 'w')
+    # TODO: validate year
 
-                f.write(r.text.encode('utf8'))
 
-                print "({0})".format(r.status_code),
-                previous_status = r.status_code
+    # Download result pages
+    url_fmt = None
+    if args.exam == 'admitere':
+        url_fmt = admitere_url_fmt
+    else:
+        url_fmt = bac_url_fmt
 
-            else:
-                print "(already downloaded)",
+    previous_status = 200
+    page_num = 1
 
-            end_time = time.clock()
-            print " - {0}s".format(end_time - start_time)
+    while previous_status == 200:
+        print '[{0}] - page {1}'.format(args.county, page_num), 
 
-            page_num += 1
+        start_time = time.clock()
+
+        url = url_fmt.format(exam=args.exam, year=args.year, county=args.county, page_num=page_num)
+        filename = file_fmt.format(exam=args.exam, year=args.year, county=args.county, page_num=page_num)
+
+        if not os.path.isfile(filename):
+            r = requests.get(url)
+            f = open(filename, 'w')
+
+            f.write(r.text.encode('utf8'))
+
+            print "({0})".format(r.status_code),
+            previous_status = r.status_code
+
+            if r.status_code != 200:
+                os.remove(filename)
+
+        else:
+            print "(already downloaded)",
+
+        end_time = time.clock()
+        print " - {0}s".format(end_time - start_time)
+
+        page_num += 1
+
+
 
